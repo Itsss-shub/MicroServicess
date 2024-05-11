acea7c7d05b012109a3c1a4233ddf6b7c1a273b4
Itsss-shub <shubhum1712@gmail.com>
2024-05-11 16:01:53 +0530

    Update Test2.java

--- GitLocalproject/src/com/Test2.java
+++ GitLocalproject/src/com/Test2.java
@@ -2,7 +2,7 @@ public class Test2{
   public static void main(String [] args){
     System.out.println("pull");
 
-    System.out.println("pull 2");
+    System.out.println("pull 2............");
   }
 
 }
