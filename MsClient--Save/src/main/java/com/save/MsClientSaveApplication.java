1b87ee2804dd6434dc350adf3ed9a1158b2035e4
lenovo <lenovo@Shubham>
2024-05-11 16:23:16 +0530

    sop statement added...

--- MsClient--Save/src/main/java/com/save/MsClientSaveApplication.java
+++ MsClient--Save/src/main/java/com/save/MsClientSaveApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class MsClientSaveApplication {
 
 	public static void main(String[] args) {
+		System.out.println("save ms");
 		SpringApplication.run(MsClientSaveApplication.class, args);
 	}
 
